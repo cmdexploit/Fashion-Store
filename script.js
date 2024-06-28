3eed4fa6849d2aeb8add6e1c18928f05ffe02a9e
cmdexploit <msanthoshkumarsrm@gmail.com>
2024-06-28 08:30:51 +0530

    completed the outline of the project

---
+++ script.js
@@ -0,0 +1,10 @@
+var sidenav=document.querySelector(".side-navbar")
+sidenav.computedStyleMap.display="none"
+  function shownav(){
+    sidenav.style.left="0"
+  }
+  function closenav(){
+    sidenav.style.left="-60%"
+  }
+
+  
